0d3cf43cfd1346113001ec94b0c67287d3cc266a
woedenaz <leminnes@gmail.com>
2021-03-19 08:45:58 -0400

    Update scraper.js

--- src/scraper.js
+++ src/scraper.js
@@ -15,8 +15,8 @@ const CONCURRENCY = 10;
 const reqPath = path.join(__dirname, "../");
 
 const URLS = [];
-let bottomNum = 1690;
-let topNum = 1710;
+let bottomNum = 2;
+let topNum = 6000;
 let totalNum = topNum - bottomNum + 1;
 let remainNum = 0;
 let acsCount = 0;
@@ -24,10 +24,10 @@ let currentSCPArray = [];
 let currentSCP = "";
 
 const barLength = 50;
-let draft = console.draft("Starting SCP Scraping");
 let barLine = console.draft(`[${" ".repeat(barLength)}] 0%`);
-let currentDoc = console.draft("Just firing her up!");
+let currentDoc = console.draft("Starting SCP Scraping");
 let errorLog = console.draft("");
+let draft = console.draft("Just Firing her Up!");
 
 for (var i = bottomNum; i >= bottomNum && i <= topNum; i++) {
 	if (i) {
@@ -46,38 +46,35 @@ let ProgressBar = (progress, length) => {
 	return `[${"=".repeat(units)}${" ".repeat(length - units)}] ${progress}%`;
 };
 
+let arrayRemove;
+
 let browser;
 let acs = [];
 const testString = /^[a-zA-Z0-9\s-]+$/;
 const scpNum = (elem) => /(?<=(scp-))[0-9]{1,4}$/.exec(elem)[0];
-const clearCurrentSCP = () => currentSCPArray = [];
 const crawlUrl = async (url) => {
 	// open a new page
-
 		const page = await browser.newPage();
 		// enter url in page
-		await page.goto(url, {timeout: 0});	
+		await page.goto(url, {waitUntil: "domcontentloaded", timeout: 0});	
 		let acsResult;
 		// errorLog(`Parsing: ${url}`);
 		remainNum ++;
 		let perNum;
 		let acsArray = [];
-		try {
-			perNum = Math.floor((remainNum / totalNum) * 100);
-		} catch (e) {
-			console.log(`cannot divide by zero:${e}`);
-		}
-		currentSCP = `SCP-${scpNum(url)}`;
-		currentSCPArray.push(currentSCP);	
+		
+		var insideSCP = `SCP-${scpNum(url)}`;
+		currentSCP = insideSCP;
+		currentSCPArray.push(insideSCP);		
 		const href = await page.evaluate(() => {
 			return document.URL;
 		});	
 		try { 
-			await page.waitForSelector("div.anom-bar-container", {timeout: 1000});
-
+            await page.waitFor(1000);
+			await page.waitForSelector("div.anom-bar-container", { timeout: 5000 });
 			
 			acsResult = await page.evaluate(() => {
-				let itemNumber = document.querySelectorAll("div.top-left-box > span.number");
+                let itemNumber = document.querySelectorAll("div.top-left-box > span.number");
 				let clearanceLevel = document.querySelectorAll("div.top-right-box > div.level");
 				let containClass = document.querySelectorAll("div.contain-class > div.class-text");					
 				let disruptClass = document.querySelectorAll("div.disrupt-class > div.class-text");
@@ -109,7 +106,6 @@ const crawlUrl = async (url) => {
 			});	
 			try {			
 				await page.screenshot({ path:`${reqPath}/screenshots/acs-pages/styled/acs-styled-page-${scpNum(url)}.png`});
-				acsCount = acsCount + 1;
 			} catch (e) {
 				errorLog(`Screenshot Exists Styled: SCP-${scpNum(url)}`);
 			}
@@ -127,42 +123,51 @@ const crawlUrl = async (url) => {
 			let risk;				
 			
 			if (containClass == true || disruptClass == true || riskClass == true) {
+				let testChilds = await page.evaluate(() => {
+					let results = document.querySelectorAll("div#page-content > *:nth-child(-n+4)");
+					return results;
+				});
+				let testChildsNum =  testChilds.length;
 				const testFind = (text) => { 
-					let testChilds = document.querySelectorAll("div#page-content > *:nth-child(-n+10)");
-					let testChildsNum =  testChilds.length;
+					let testBool;	
+					let testText = text;					
 					for (let j = testChildsNum; j < testChildsNum; j++) {
-						testChilds[j].innerText.toLowerCase().includes(text);
+						let testChildInner = testChilds[j].innerText.toLowerCase();
+						testBool = testChildInner.includes(testText);
 					}
+					return testBool ? testBool : null;		
 				};
-				const urClass = await page.evaluate(testFind("unrestricted"));			
-				const rsClass = await page.evaluate(testFind("restricted"));
-				const cfClass = await page.evaluate(testFind("confidential"));
-				const scClass = await page.evaluate(testFind("secret"));
-				const tsClass = await page.evaluate(testFind("top secret"));
-				const ctsClass = await page.evaluate(testFind("cosmic top secret"));
-				const darkClass = await page.evaluate(testFind("dark"));				
-				const vlamClass = await page.evaluate(testFind("vlam"));
-				const keneqClass = await page.evaluate(testFind("keneq"));
-				const ekhiClass = await page.evaluate(testFind("ekhi"));
-				const amidaClass = await page.evaluate(testFind("amida"));
-				const noticeClass = await page.evaluate(testFind("notice"));
-				const cautionClass = await page.evaluate(testFind("caution"));
-				const warningClass = await page.evaluate(testFind("warning"));
-				const dangerClass = await page.evaluate(testFind("danger"));
-				const criticalClass = await page.evaluate(testFind("critical"));				
 
+				let	urClass = testFind("unrestricted");				
+				let	rsClass = testFind("restricted");				
+				let	cfClass = testFind("confidential");				
+				let	scClass = testFind("secret");				
+				let	tsClass = testFind("top secret");				
+				let	ctsClass = testFind("cosmic top secret");				
+				let	darkClass = testFind("dark");						
+				let	vlamClass = testFind("vlam");				
+				let	keneqClass = testFind("keneq");				
+				let	ekhiClass = testFind("ekhi");			
+				let	amidaClass = testFind("amida");				
+				let	noticeClass = testFind("notice");				
+				let	cautionClass = testFind("caution");				
+				let	warningClass = testFind("warning");				
+				let	dangerClass = testFind("danger");				
+				let	criticalClass = testFind("critical");	
+
+				let containContainer;	
+				let secondContainer;
+				
 				try {
-					itemNum = await page.evaluate(() => document.querySelector("div#page-title").textContent.trim());						
+					itemNum = `${insideSCP}`;						
 				} catch (e) {
-					// (`Item # Error:${e}`);
-				}
-
-				let containContainer;								
+					console.log(`Item # Error:${e}`);
+				}							
 
 				try {
-					contain = await page.evaluate(() => {					
-						containContainer = document
-							.evaluate(
+					contain = await page.evaluate(() => {
+						try {					
+							containContainer = document.evaluate(
 								"/html/body//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'),'containment class')]",
 								document,
 								null,
@@ -170,111 +175,69 @@ const crawlUrl = async (url) => {
 								null
 							)
 							.singleNodeValue;
+						} catch (e) {
+							console.log(`contain single value Error:${e}`);
+						}
 						
-							if (
-								containContainer.nextSibling 
-								&& 
-								containContainer.nextSibling.textContent.length > 1
-								&& 
-								testString.test(containContainer.nextSibling.textContent.trim())) {
-								return containContainer.nextSibling.textContent.trim();
-							} else if (
-								containContainer.nextSibling 
-								&& 
-								containContainer.nextSibling.textContent.length > 1
-								&& 
-								testString.test(containContainer.nextElementSibling.textContent.trim())) {
-								return containContainer.nextElementSibling.textContent.trim();
-							} else if (
-								containContainer.parentNode.childNodes[1] 
-								&& 
-								containContainer.parentNode.childNodes[1].textContent.length > 1
-								&& 
-								testString.test(containContainer.parentNode.childNodes[1].textContent.trim())) {
-								return containContainer.parentNode.childNodes[1].textContent.trim();
-							} else if (
-								containContainer.parentNode.childNodes[2] 
-								&& 
-								containContainer.parentNode.childNodes[2].textContent.length > 1
-								&& 
-								testString.test(containContainer.parentNode.childNodes[2].textContent.trim())) {
-								return containContainer.parentNode.childNodes[2].textContent.trim();
-							} else if (
-								containContainer.parentNode.parentNode.childNodes[1] 
-								&& 
-								containContainer.parentNode.parentNode.childNodes[1].textContent.length > 1
-								&& 
-								testString.test(containContainer.parentNode.parentNode.childNodes[1].textContent.trim())) {
-								return containContainer.parentNode.parentNode.childNodes[1].textContent.trim();
-							} else if (
-								containContainer.parentNode.parentNode.childNodes[2] 
-								&& 
-								containContainer.parentNode.parentNode.childNodes[2].textContent.length > 1
-								&& 
-								testString.test(containContainer.parentNode.parentNode.childNodes[2].textContent.trim())) {
-								return containContainer.parentNode.parentNode.childNodes[2].textContent.trim();
-							}								
+						if (
+							containContainer.nextSibling && containContainer.nextSibling.textContent.length > 1 && testString.test(containContainer.nextSibling.textContent.trim())) {
+							return containContainer.nextSibling.textContent.trim();
+						} else if (
+							containContainer.nextSibling && containContainer.nextSibling.textContent.length > 1 && testString.test(containContainer.nextElementSibling.textContent.trim())) {
+							return containContainer.nextElementSibling.textContent.trim();
+						} else if (
+							containContainer.parentNode.childNodes[1] && containContainer.parentNode.childNodes[1].textContent.length > 1 && testString.test(containContainer.parentNode.childNodes[1].textContent.trim())) {
+							return containContainer.parentNode.childNodes[1].textContent.trim();
+						} else if (
+							containContainer.parentNode.childNodes[2] && containContainer.parentNode.childNodes[2].textContent.length > 1 && testString.test(containContainer.parentNode.childNodes[2].textContent.trim())) {
+							return containContainer.parentNode.childNodes[2].textContent.trim();
+						} else if (
+							containContainer.parentNode.parentNode.childNodes[1] && containContainer.parentNode.parentNode.childNodes[1].textContent.length > 1 && testString.test(containContainer.parentNode.parentNode.childNodes[1].textContent.trim())) {
+							return containContainer.parentNode.parentNode.childNodes[1].textContent.trim();
+						} else if (
+							containContainer.parentNode.parentNode.childNodes[2] && containContainer.parentNode.parentNode.childNodes[2].textContent.length > 1 && testString.test(containContainer.parentNode.parentNode.childNodes[2].textContent.trim())) {
+							return containContainer.parentNode.parentNode.childNodes[2].textContent.trim();
+						}							
 					});						
 				} catch (e) {								
 					contain = "none";
 				}							
 
 				try {
-					second = await page.evaluate(() => {					
-						let secondContainer = document
-							.evaluate(
-								// eslint-disable-next-line quotes
-								'/html/body//text()[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz"),"secondary class")]',
-								document,
-								null,
-								XPathResult.FIRST_ORDERED_NODE_TYPE,
-								null
-							)
-							.singleNodeValue;
-						
-							if (
-								secondContainer.nextSibling 
-								&& 
-								secondContainer.nextSibling.textContent.length > 1
-								&& 
-								testString.test(secondContainer.nextSibling.textContent.trim())) {
-								return secondContainer.nextSibling.textContent.trim();
-							} else if (
-								secondContainer.nextSibling 
-								&& 
-								secondContainer.nextSibling.textContent.length > 1
-								&& 
-								testString.test(secondContainer.nextElementSibling.textContent.trim())) {
-								return secondContainer.nextElementSibling.textContent.trim();
-							} else if (
-								secondContainer.parentNode.childNodes[1] 
-								&& 
-								secondContainer.parentNode.childNodes[1].textContent.length > 1
-								&& 
-								testString.test(secondContainer.parentNode.childNodes[1].textContent.trim())) {
-								return secondContainer.parentNode.childNodes[1].textContent.trim();
-							} else if (
-								secondContainer.parentNode.childNodes[2] 
-								&& 
-								secondContainer.parentNode.childNodes[2].textContent.length > 1
-								&& 
-								testString.test(secondContainer.parentNode.childNodes[2].textContent.trim())) {
-								return secondContainer.parentNode.childNodes[2].textContent.trim();
-							} else if (
-								secondContainer.parentNode.parentNode.childNodes[1] 
-								&& 
-								secondContainer.parentNode.parentNode.childNodes[1].textContent.length > 1
-								&& 
-								testString.test(secondContainer.parentNode.parentNode.childNodes[1].textContent.trim())) {
-								return secondContainer.parentNode.parentNode.childNodes[1].textContent.trim();
-							} else if (
-								secondContainer.parentNode.parentNode.childNodes[2] 
-								&& 
-								secondContainer.parentNode.parentNode.childNodes[2].textContent.length > 1
-								&& 
-								testString.test(secondContainer.parentNode.parentNode.childNodes[2].textContent.trim())) {
-								return secondContainer.parentNode.parentNode.childNodes[2].textContent.trim();
-							}								
+					second = await page.evaluate(() => {
+						try {					
+							secondContainer = document
+								.evaluate(
+									// eslint-disable-next-line quotes
+									'/html/body//text()[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz"),"secondary class")]',
+									document,
+									null,
+									XPathResult.FIRST_ORDERED_NODE_TYPE,
+									null
+								)
+								.singleNodeValue;
+						} catch (e) {
+							console.log(`second single value Error:${e}`);
+						}						
+						if (
+							secondContainer.nextSibling && secondContainer.nextSibling.textContent.length > 1 && testString.test(secondContainer.nextSibling.textContent.trim())) {
+							return secondContainer.nextSibling.textContent.trim();
+						} else if (
+							secondContainer.nextSibling && secondContainer.nextSibling.textContent.length > 1 && testString.test(secondContainer.nextElementSibling.textContent.trim())) {
+							return secondContainer.nextElementSibling.textContent.trim();
+						} else if (
+							secondContainer.parentNode.childNodes[1] && secondContainer.parentNode.childNodes[1].textContent.length > 1 && testString.test(secondContainer.parentNode.childNodes[1].textContent.trim())) {
+							return secondContainer.parentNode.childNodes[1].textContent.trim();
+						} else if (
+							secondContainer.parentNode.childNodes[2] && secondContainer.parentNode.childNodes[2].textContent.length > 1 && testString.test(secondContainer.parentNode.childNodes[2].textContent.trim())) {
+							return secondContainer.parentNode.childNodes[2].textContent.trim();
+						} else if (
+							secondContainer.parentNode.parentNode.childNodes[1] && secondContainer.parentNode.parentNode.childNodes[1].textContent.length > 1 && testString.test(secondContainer.parentNode.parentNode.childNodes[1].textContent.trim())) {
+							return secondContainer.parentNode.parentNode.childNodes[1].textContent.trim();
+						} else if (
+							secondContainer.parentNode.parentNode.childNodes[2] && secondContainer.parentNode.parentNode.childNodes[2].textContent.length > 1 && testString.test(secondContainer.parentNode.parentNode.childNodes[2].textContent.trim())) {
+							return secondContainer.parentNode.parentNode.childNodes[2].textContent.trim();
+						}								
 					});						
 				} catch (e) {								
 					second = "none";						
@@ -318,25 +281,37 @@ const crawlUrl = async (url) => {
 				} catch (e) {
 					errorLog(`Screenshot Exists Plain: SCP-${scpNum(url)}`);
 				}
-				acsCount = acsCount + 1;
-			} else {
-				try {
-					await page.screenshot({ path:`${reqPath}/screenshots/error-pages/error-page-${scpNum(url)}.png`});
-				} catch (e) {
-					errorLog(`Screenshot Exists Error: SCP-${scpNum(url)}`);					
-				}				
 			}
 		}	
 	
-	await currentDoc(`Current Document Being Evaluated: ${href}`);						
-	acs.push(acsResult);
-	await page.close();
-	let index = currentSCPArray.indexOf(currentSCP);
-	if (index > -1) {
-		currentSCPArray.splice(index, 1);
+	await currentDoc(`Current Document Being Evaluated: ${href}`);
+	if (acsResult) {	
+		if (!(!acsResult.clearance || acsResult.clearance == "none") && !(!acsResult.contain || acsResult.contain == "none") && !(!acsResult.secondary || acsResult.secondary == "none") && !(!acsResult.disrupt || acsResult.disrupt == "none") && !(!acsResult.risk || acsResult.risk == "none")) {		
+			// console.log(`acs: ${JSON.stringify(acs, null, 4)} | acsResult: ${JSON.stringify(acsResult, null, 4)}`);
+			// console.log(`acs type: ${typeof acs} | acsResult type: ${typeof acsResult}`);
+			acsCount = acsCount + 1;
+			acs.push(acsResult);
+		}
 	}
-	draft(`% done: ${perNum} | ACS Count: ${acsCount} | Current SCPs: ${currentSCPArray.toString()}`);
+	await page.close();
+	let index = currentSCPArray.indexOf(insideSCP);
+	try {
+		perNum = Math.floor((remainNum / totalNum) * 100);
+	} catch (e) {
+		console.log(`cannot divide by zero:${e}`);
+	}
+	if (currentSCPArray[index]) {
+		arrayRemove = (arr, value) => { 
+			return arr.filter(function(ele){ 
+				return ele != value; 
+			});
+		};
+		errorLog(`index: ${index} | ${currentSCPArray[index]}`);
+		currentSCPArray = arrayRemove(currentSCPArray, currentSCPArray[index]);
+	}
 	barLine(ProgressBar(perNum, barLength));
+	draft(`% done: ${perNum} | ACS Count: ${acsCount} 
+Current SCPs: ${currentSCPArray.toString()}`);
 };
 
 const promiseProducer = () => {
@@ -371,24 +346,19 @@ const mainFunc = async () => {
 
     // Runs thru all the urls in a pool of given concurrency.
     const pool = new PromisePool(promiseProducer, CONCURRENCY);
-	try {
-		clearCurrentSCP();
-		await pool.start();
-	} catch (e) {
-		errorLog(`
-Promise Pool Error: 
-Error: ${e}
-Pool: ${JSON.stringify(pool)}
-Current SCP: ${currentSCP}`);
-	}
+	await pool.start();
     
+    browser.close();
     // Print results.
     let acsFiltered = acs.filter((el) => el != null);
-	fs.writeFileSync(`${reqPath}/acs-database.json`, JSON.stringify(acsFiltered, null, 4), (e) => {
-		if (e) throw e;
-	});
-    
-    await browser.close();
+    let acsJSON = JSON.stringify(acsFiltered, null, 4);
+    fs.writeFileSync(`${reqPath}/acs-database.json`, acsJSON, "utf8",{ flag: "wx" }, (e) => {
+        if (e) {
+            return console.log(e);
+        }
+        console.log("The file was saved!");
+    });
 };
 
 mainFunc();
+console.clear();
